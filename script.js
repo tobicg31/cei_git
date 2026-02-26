ef722a70f9f65e2f279bcbc0ab17eb406aeb452f
tobicg31 <82407605+tobicg31@users.noreply.github.com>
2026-02-26 13:35:39 -0300

    gchhfc

---
+++ script.js
@@ -0,0 +1,31 @@
+document.querySelectorAll('.secretaria-card').forEach(card => {
+    card.addEventListener('click', function() {
+
+        const title = this.getAttribute('data-title');
+        const text = this.getAttribute('data-text');
+
+        document.getElementById('modalTitle').textContent = title;
+        document.getElementById('modalText').textContent = text;
+
+        const modal = new bootstrap.Modal(document.getElementById('secretariaModal'));
+        modal.show();
+    });
+});
+
+const sections = document.querySelectorAll('.fade-normal, .fade-right, .fade-left');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show'); // ← esto hace que se resetee
+        }
+    });
+}, {
+    threshold: 0.25
+});
+
+sections.forEach(section => {
+    observer.observe(section);
+});
